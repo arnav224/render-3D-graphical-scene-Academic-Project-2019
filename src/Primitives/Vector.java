c4270ed9a0a3b76f754b2b06b4778a07e017d4ac
יעקב ברין <ybrin@g.jct.ac.il>
2019-03-24 13:43:28 +0200

    התאמת הפונקציות לדרישה

--- src/Primitives/Vector.java
+++ src/Primitives/Vector.java
@@ -1,4 +1,5 @@
 package Primitives;
+import static Primitives.Util.*;
 
 public class Vector implements Comparable<Vector>{
     private Point3D _head;
@@ -69,7 +70,7 @@ public class Vector implements Comparable<Vector>{
      **************************************************/
 
     public void add (Vector vector ) {
-        this._head = this._head.add(vector);
+        this._head.add(vector);// = this._head.add(vector);
     }
     //public Vector add (Vector vector ){
     //    return new Vector(this._head.add(vector));
@@ -86,7 +87,7 @@ public class Vector implements Comparable<Vector>{
      * subtract method in Point3D class.
      **************************************************/
     public void subtract (Vector vector){
-        this._head = this._head.subtract(vector);
+        this._head.subtract(vector);
     }
     /*************************************************
      * FUNCTION
@@ -99,9 +100,9 @@ public class Vector implements Comparable<Vector>{
      * scale method in Coordinate class.
      **************************************************/
     public void scale(double scalingFactor){
-        this._head = new Point3D(_head.getX().scale(scalingFactor),
-                _head.getY().scale(scalingFactor),
-                _head.getZ().scale(scalingFactor));
+        this._head = new Point3D(uscale(_head.getX().getCoordinate(), scalingFactor),
+                uscale(_head.getY().getCoordinate(), scalingFactor),
+                uscale(_head.getZ().getCoordinate(), scalingFactor));
     }
     /*************************************************
      * FUNCTION
@@ -113,13 +114,20 @@ public class Vector implements Comparable<Vector>{
      * MEANING
      * cross product of 2 vectors.
      * SEE ALSO
-     * multiply method in Coordinate class.
+     * usubtract, uscale in Util class.
      **************************************************/
+    public Vector crossProduct(Vector vector){
+        return new Vector(usubtract(uscale(this._head.getY().getCoordinate(), (vector._head.getZ().getCoordinate())), uscale(this._head.getZ().getCoordinate(), (vector._head.getY().getCoordinate()))),
+                usubtract(uscale(this._head.getZ().getCoordinate(), (vector._head.getX().getCoordinate())), uscale(this._head.getX().getCoordinate(), (vector._head.getZ().getCoordinate()))),
+                usubtract(uscale(this._head.getX().getCoordinate(), (vector._head.getY().getCoordinate())), uscale(this._head.getY().getCoordinate(), (vector._head.getX().getCoordinate()))));
+    }
+/*
     public Vector crossProduct(Vector vector){
         return new Vector(this._head.getY().multiply(vector._head.getZ()).subtract(this._head.getZ().multiply(vector._head.getY())).getCoordinate(),
                 this._head.getZ().multiply(vector._head.getX()).subtract(this._head.getX().multiply(vector._head.getZ())).getCoordinate(),
                 this._head.getX().multiply(vector._head.getY()).subtract(this._head.getY().multiply(vector._head.getX())).getCoordinate());
     }
+*/
     /*************************************************
      * FUNCTION
      * length
@@ -155,8 +163,8 @@ public class Vector implements Comparable<Vector>{
      * multiply method in Coordinate class.
      **************************************************/
     public double dotProduct(Vector o){
-        return this._head.getX().multiply(o._head.getX()).getCoordinate() +
-                this._head.getY().multiply(o._head.getY()).getCoordinate() +
-                this._head.getZ().multiply(o._head.getZ()).getCoordinate();
+        return uscale(this._head.getX().getCoordinate(), (o._head.getX()).getCoordinate()) +
+                uscale(this._head.getY().getCoordinate(), (o._head.getY()).getCoordinate()) +
+                uscale(this._head.getZ().getCoordinate(), (o._head.getZ()).getCoordinate());
     }
 }
