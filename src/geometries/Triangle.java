93eeb4d8493760237c99bccdf83dc7680b6164b8
אברהם פרנקל <avraham224@gmail.com>
2019-03-31 23:15:31 +0300

    2תחילת geometries

--- src/geometries/Triangle.java
+++ src/geometries/Triangle.java
@@ -1,6 +1,9 @@
 package geometries;
 import Primitives.*;
 
+import java.util.List;
+import java.util.Map;
+
 public class Triangle extends Geometry {
 
     private Point3D _p1;
@@ -9,18 +12,57 @@ public class Triangle extends Geometry {
 
     // ***************** Constructors ********************** //
 
-//    public Triangle();
-//    public Triangle(Triangle triangle);
-//    public Triangle(Point3D p1, Point3D p2, Point3D p3);
-//    public Triangle(Map<String, String> attributes);
+    public Triangle() {
+        this._p1 = new Point3D();
+        this._p2 = new Point3D();
+        this._p3 = new Point3D();
+    }
+    public Triangle(Triangle triangle){
+        this._p1 = triangle._p1;
+        this._p2 = triangle._p2;
+        this._p3 = triangle._p3;
+    }
+    public Triangle(Point3D _p1, Point3D _p2, Point3D _p3) {
+        this._p1 = _p1;
+        this._p2 = _p2;
+        this._p3 = _p3;
+    }
+    public Triangle(Map<String, String> attributes){
+        //todo implement
+    }
+
     // ***************** Getters/Setters ********************** //
-//    public Point3D getP1();
-//    public Point3D getP2();
-//    public Point3D getP3();
-//    public void setP1(Point3D p1);
-//    public void setP2(Point3D p2);
-//    public void setP3(Point3D p3);
-//    // ***************** Operations ******************** //
-//    public Vector getNormal(Point3D point);
-//    public List<Point3D> FindIntersections(Ray ray);
+    public Point3D getP1() {
+        return _p1;
+    }
+    public Triangle setP1(Point3D _p1) {
+        this._p1 = _p1;
+        return this;
+    }
+    public Point3D getP2() {
+        return _p2;
+    }
+    public Triangle setP2(Point3D _p2) {
+        this._p2 = _p2;
+        return this;
+    }
+    public Point3D getP3() {
+        return _p3;
+    }
+    public Triangle setP3(Point3D _p3) {
+        this._p3 = _p3;
+        return this;
+    }
+
+    // ***************** Operations ******************** //
+    @Override
+    public Vector getNormal(Point3D point) {
+        //todo implement
+        return null;
+    }
+    @Override
+    public List<Point3D> FindIntersections(Ray ray) {
+        //todo implement
+        return null;
+    }
 }
