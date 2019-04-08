4ddd94b4d7e640319667b474488a8cc253a6d832
אברהם פרנקל <avraham224@gmail.com>
2019-04-08 13:02:00 +0300

    camera

--- src/Primitives/Ray.java
+++ src/Primitives/Ray.java
@@ -17,6 +17,11 @@ public class Ray {
         this._direction = new Vector(ray.getDirection());
     }
 
+    public Ray(Point3D _POO, Vector _direction) {
+        this._POO = new Point3D(_POO);
+        this._direction = new Vector(_direction);
+    }
+
     // ***************** Getters/Setters ********************** //
     public Point3D getPOO() {
         return new Point3D(_POO);
