afcbee8c1966859144408589a43454a8667739cf
אברהם פרנקל <avraham224@gmail.com>
2019-05-13 11:59:14 +0300

    הגשה שניה .

--- Tests/renderer/RenderTest.java
+++ Tests/renderer/RenderTest.java
@@ -193,4 +193,62 @@ class RenderTest {
     @Test
     void printGrid() {
     }
+    @Test
+    public void basicRendering() {
+
+//        new File(IMAGES_TEST_DIR);
+        Scene scene = new Scene();
+        scene.setScreenDistance(50);
+
+        /*scene.set_camera(new Camera(
+                new Point3D(0,0,10),
+                new Vector(0,0,-1),
+                new Vector(0,1,0)));
+           scene.set_background(new Color(0.3f ,0.5f, 0.74f));*/
+//         scene.set_ambientLight(new AmbientLight(Color.BLACK);
+
+        Sphere sphere = new Sphere(50, new Point3D(0.0, 0.0, -50));
+        scene.addGeometry(sphere);
+
+        Triangle triangle1 = new Triangle(
+                new Point3D(100, 0, -49),
+                new Point3D(0, 100, -49),
+                new Point3D(100, 100, -49));
+
+        Triangle triangle2 = new Triangle(
+                new Point3D(100, 0, -49),
+                new Point3D(0, -100, -49),
+                new Point3D(100, -100, -49));
+
+        Triangle triangle3 = new Triangle(
+                new Point3D(-100, 0, -49),
+                new Point3D(0, 100, -49),
+                new Point3D(-100, 100, -49));
+
+        Triangle triangle4 = new Triangle(
+                new Point3D(-100, 0, -49),
+                new Point3D(0, -100, -49),
+                new Point3D(-100, -100, -49));
+
+        sphere.setEmmission( new Color(255,152,187));
+        triangle1.setEmmission( new Color(255, 31, 26));
+        triangle2.setEmmission( new Color(23, 247, 255));
+        triangle3.setEmmission( new Color(255, 254, 0));
+        triangle4.setEmmission( new Color(34, 255, 0));
+
+        scene.addGeometry(triangle1);
+        scene.addGeometry(triangle2);
+        scene.addGeometry(triangle3);
+        scene.addGeometry(triangle4);
+
+        ImageWriter imageWriter = new ImageWriter( "Render test efi", 500, 500, 500, 500);
+
+        Render render = new Render(imageWriter, scene);
+
+        render.renderImage();
+        render.printGrid(50);
+        render.writeToImage();
+
+    }
+
 }
